6661c4d122327a0c1693fc3b24d13ae4961b9d1a
Eliana <114009849+EliGaraventa@users.noreply.github.com>
2024-11-04 13:34:16 -0300

    Update tp1rec.js

--- tp1rec/tp1rec.js
+++ tp1rec/tp1rec.js
@@ -51,10 +51,26 @@ function draw() {
     pantallaMuerte();
     return;
   }
-  
+
+  if (rama === 0) {
+    image(inicio[ramaPos], 0, 0, width, height);
+    text(InicioTexto[ramaPos], 0, 50, width);
+    siguiente = true;
+
+    if (ramaPos === 2 || ramaPos === 6 || ramaPos === 9 || ramaPos === 13 || ramaPos === 17 || ramaPos === 24 || ramaPos === 27) {
+      decision = true;
+      pantallaDecision();
+    } else if (ramaPos === 30) {
+      muerte = true;
+      pantallaMuerte();
+      siguiente = false;
+    } else {
+      botonSiguiente();
+    }
+  }
+
   if (rama === 1) {
     image(rama1[ramaPos], 0, 0, width, height);
-    textAlign(CENTER, TOP);
     text(rama1dialogos[ramaPos], 0, 50, width);
     if (ramaPos === 3) {
       decision = true;
@@ -72,7 +88,6 @@ function draw() {
 
   if (rama === 2) {
     image(rama1_2[ramaPos], 0, 0, width, height);
-    textAlign(CENTER, TOP);
     text(rama1_2dialogos[ramaPos], 0, 50, width);
     botonSiguiente();
 
@@ -89,24 +104,22 @@ function draw() {
   }
   if (rama === 3) {
     image(rama1_3[ramaPos], 0, 0, width, height);
-    textAlign(CENTER, TOP);
     text(rama1_3dialogos[ramaPos], 0, 50, width);
     botonSiguiente();
     if (ramaPos === 4 && rama === 3) {
       muerte = true;
       pantallaMuerte();
       siguiente = false;
-    } if ( ramaPos === 6){
+    }
+    if ( ramaPos === 6) {
       pantallaMuerte();
       siguiente = false;
-      
     }
     return;
   }
-  
+
   if (rama === 4) {
     image(rama2a[ramaPos], 0, 0, width, height);
-    textAlign(CENTER, TOP);
     text(rama2a_dialogos[ramaPos], 0, 50, width);
     botonSiguiente();
 
@@ -125,7 +138,6 @@ function draw() {
 
   if (rama === 5) {
     image(rama2_1[ramaPos], 0, 0, width, height);
-    textAlign(CENTER, TOP);
     text(rama2_1dialogo[ramaPos], 0, 50, width);
     botonSiguiente();
     if (ramaPos !== 4 && ramaPos !== 8) {
@@ -146,7 +158,6 @@ function draw() {
   }
   if (rama === 6) {
     image(rama2_2[ramaPos], 0, 0, width, height);
-    textAlign(CENTER, TOP);
     text(rama2_2dialogo[ramaPos], 0, 50, width);
     botonSiguiente();
     if (ramaPos === 4) {
@@ -158,7 +169,6 @@ function draw() {
   }
   if (rama === 7) {
     image(rama3[ramaPos], 0, 0, width, height);
-    textAlign(CENTER, TOP);
     text(rama3dialogo[ramaPos], 0, 50, width);
     botonSiguiente();
     if (ramaPos === 1) {
@@ -170,7 +180,6 @@ function draw() {
   }
   if (rama === 8) {
     image(rama4[ramaPos], 0, 0, width, height);
-    textAlign(CENTER, TOP);
     text(rama4dialogo[ramaPos], 0, 50, width);
     botonSiguiente();
     if (ramaPos === 1) {
@@ -181,9 +190,8 @@ function draw() {
     return;
   }
 
-if (rama === 9) {
+  if (rama === 9) {
     image(rama5[ramaPos], 0, 0, width, height);
-    textAlign(CENTER, TOP);
     text(rama5dialogo[ramaPos], 0, 50, width);
     botonSiguiente();
     if (ramaPos === 2 && rama === 9) {
@@ -193,24 +201,6 @@ if (rama === 9) {
     }
     return;
   }
-
-  if (rama === 0) {
-  image(inicio[ramaPos], 0, 0, width, height);
-  textAlign(CENTER, TOP);
-  text(InicioTexto[ramaPos], 0, 50, width);
-  siguiente = true;
-
-  if (ramaPos === 2 || ramaPos === 6 || ramaPos === 9 || ramaPos === 13 || ramaPos === 17 || ramaPos === 24 || ramaPos === 27) {
-    decision = true;
-    pantallaDecision();
-  } else if (ramaPos === 30) {
-    muerte = true;
-    pantallaMuerte();
-    siguiente = false;
-  } else {
-    botonSiguiente();
-  }
-}
 }
 
 function botonSiguiente() {
@@ -222,11 +212,11 @@ function botonSiguiente() {
     fill(255);
   }
 }
-  
+
 function mousePressed() {
-  
+
   if (!musicaIniciada) {
-    sound.loop();  
-    musicaIniciada = true;  
+    sound.loop();
+    musicaIniciada = true;
   }
 }
